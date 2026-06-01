0fe6880820ab9f5c684b9b210f7409bb2c893e44
Sanjay G <gokulesh2615@gmail.com>
2026-06-01 16:59:34 +0530

    feat(errors): implement typed exception hierarchy

---
+++ packages/core/exceptions.py
@@ -0,0 +1,7 @@
+class MemoryException(Exception):
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(message)
+
+    def __str__(self) -> str:
+        return self.message
