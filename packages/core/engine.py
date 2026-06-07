077afb2413d29bdb3255ad85430afd4fba576795
Sanjay G <gokulesh2615@gmail.com>
2026-06-07 17:13:28 +0530

    perf(core): add caching to the memory engine

--- packages/core/engine.py
+++ packages/core/engine.py
@@ -2,6 +2,7 @@ import logging
 from typing import List, Dict
 from .types import MemoryModel
 from .exceptions import MemoryException
+import threading
 
 logger = logging.getLogger(__name__)
 
@@ -10,11 +11,17 @@ class MemoryEngine:
         self.memory_model = memory_model
         self.memory_store = {}
         self.lock = threading.Lock()
+        self.cache = {}
+        self.cache_size = 100
+        self.hits = 0
+        self.misses = 0
 
     def store(self, key: str, value: str) -> None:
         with self.lock:
             try:
                 self.memory_store[key] = value
+                if len(self.cache) < self.cache_size:
+                    self.cache[key] = value
                 logger.info(f'Stored key {key} with value {value}')
             except Exception as e:
                 logger.error(f'Error storing key {key}: {str(e)}')
@@ -23,12 +30,18 @@ class MemoryEngine:
     def retrieve(self, key: str) -> str:
         with self.lock:
             try:
-                value = self.memory_store.get(key)
-                if value is None:
+                if key in self.cache:
+                    self.hits += 1
+                    return self.cache[key]
+                elif key in self.memory_store:
+                    self.misses += 1
+                    value = self.memory_store[key]
+                    if len(self.cache) < self.cache_size:
+                        self.cache[key] = value
+                    return value
+                else:
                     logger.error(f'Key {key} not found')
                     raise MemoryException(f'Key {key} not found')
-                logger.info(f'Retrieved key {key} with value {value}')
-                return value
             except Exception as e:
                 logger.error(f'Error retrieving key {key}: {str(e)}')
                 raise MemoryException(f'Error retrieving key {key}: {str(e)}')
@@ -38,6 +51,8 @@ class MemoryEngine:
             try:
                 if key in self.memory_store:
                     del self.memory_store[key]
+                    if key in self.cache:
+                        del self.cache[key]
                     logger.info(f'Deleted key {key}')
                 else:
                     logger.error(f'Key {key} not found')
@@ -51,6 +66,8 @@ class MemoryEngine:
             try:
                 if key in self.memory_store:
                     self.memory_store[key] = value
+                    if key in self.cache:
+                        self.cache[key] = value
                     logger.info(f'Updated key {key} with value {value}')
                 else:
                     logger.error(f'Key {key} not found')
